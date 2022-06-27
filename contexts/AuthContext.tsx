30249475e4539152624a6973cff7c9de1c468ab4
Sadrak Morais <sadrak.morais@gcbinvestimentos.com>
2022-06-27 11:03:17 -0300

    feat: 🛠  Save data on cookies with Nookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import Router from "next/router";
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
+import { setCookie, parseCookies } from "nookies";
 
 type SignInCredentials = {
   email: string;
@@ -28,6 +29,17 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User | null>(null);
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { nextAuthToken: token } = parseCookies();
+
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
+
   const signIn = async ({ email, password }: SignInCredentials) => {
     try {
       const response = await api.post("sessions", {
@@ -35,7 +47,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
         password,
       });
 
-      const { permissions, roles } = response.data;
+      const { token, permissions, roles, refreshToken } = response.data;
+
+      setCookie(undefined, "nextAuthToken", token, {
+        maxAge: 60 * 60 * 24 * 30, //30 dias
+        path: "/",
+      });
+      setCookie(undefined, "nextAuthRefreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, //30 dias
+        path: "/",
+      });
 
       setUser({
         email,
@@ -43,6 +64,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         roles,
       });
 
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
       Router.push("/dashboard");
     } catch (error) {
       console.log(error);
